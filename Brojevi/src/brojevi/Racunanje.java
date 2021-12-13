2fd434942197863f5c5ff165503b4e034d59dc9b
JankovicFilip <93282802+JankovicFilip@users.noreply.github.com>
2021-12-13 18:26:31 +0100

    Dijeljenje

--- Brojevi/src/brojevi/Racunanje.java
+++ Brojevi/src/brojevi/Racunanje.java
@@ -5,6 +5,8 @@ public class Racunanje {
 	public static void main(String[] args) {
 		int result = 10+3;
 		System.out.println(result);
+		double result2= (double)10 / (double)3;
+		System.out.println(result2);
 
 	}
 
