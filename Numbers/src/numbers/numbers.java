e17453e40e57a85d598fc9c78d18a38c18a82930
JankovicFilip <93282802+JankovicFilip@users.noreply.github.com>
2021-12-13 18:21:41 +0100

    Brojevi i arrays u Javi

---
+++ Numbers/src/numbers/numbers.java
@@ -0,0 +1,10 @@
+package numbers;
+
+public class Main {
+	public static void main(String[] args) {
+		int [][] numbers = new int [2][3];
+		numbers [0][0] = 1;
+		System.out.println(Arrays.toString(numbers));
+	}
+
+}
