4449243aaebe516d8a7693fad26c71a2ab57be41
JankovicFilip <93282802+JankovicFilip@users.noreply.github.com>
2021-12-18 19:40:35 +0100

    Komentiranje if, else if, else linija i stvaljanje sve to u inline komandu

--- Edunova02/src/edunova/UcenjeKomandi.java
+++ Edunova02/src/edunova/UcenjeKomandi.java
@@ -11,15 +11,19 @@ public class UcenjeKomandi {
 				JOptionPane.showInputDialog("Koliko dugo radis na ovoj poziciji")
 				);
 		
-		if (dob >= 2000 && rad<=2) {
-			System.out.println("Oprosti nemas dovoljno iskustva");
-		}else if (dob >= 1990 && dob <= 1999 && rad>3 && rad <5) {
-			System.out.println("Imamo poziciju za vas kao middle developera");
-		}else if (dob >= 1970 && dob <=1989 && rad >=5) {
-			System.out.println("Rado bismo vas zaposlili na nekoj bitnijoj poziciji");
-		}else {
-			System.out.println("Molim vas da nam podijelite vise informacija");
-		}
+		System.out.println((dob >= 2000 && rad<=2) ? "Oprosti nemas dovoljno iskustva" : 
+			(dob >= 1990 && dob <= 1999 && rad>3 && rad <5) ? "Imamo poziciju za vas kao middle developer" : 
+			(dob >= 1970 && dob <=1989 && rad >=5) ? "Rado bismo vas zaposlili na nekoj bitnijoj poziciji" : "Molim vas podijelite nam vise informacija o vama");
+		
+		//if (dob >= 2000 && rad<=2) {
+			//System.out.println("Oprosti nemas dovoljno iskustva");
+		//}else if (dob >= 1990 && dob <= 1999 && rad>3 && rad <5) {
+			//System.out.println("Imamo poziciju za vas kao middle developera");
+		//}else if (dob >= 1970 && dob <=1989 && rad >=5) {
+			//System.out.println("Rado bismo vas zaposlili na nekoj bitnijoj poziciji");
+		//}else {
+			//System.out.println("Molim vas da nam podijelite vise informacija");
+		//}
 		
  	}
 
