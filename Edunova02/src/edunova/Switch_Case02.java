ca500f290c4bf131f0ae61589da9cdaf15859a14
JankovicFilip <93282802+JankovicFilip@users.noreply.github.com>
2021-12-20 15:08:57 +0100

    Ucenje switch i case i monthString i da sve to ispise u sysout

--- Edunova02/src/edunova/Switch_Case02.java
+++ Edunova02/src/edunova/Switch_Case02.java
@@ -1,5 +1,7 @@
 package edunova;
 
+import java.time.Month;
+
 import javax.swing.JOptionPane;
 
 public class Switch_Case02 {
@@ -8,15 +10,57 @@ public class Switch_Case02 {
 		int mjesec = Integer.parseInt(
 				JOptionPane.showInputDialog("Koji mjesec Vam je najdraži?")
 				);
+		String monthString = null;
+		
 		// tu napraviti joption godina
-		int godina = 1993;
+		int godina = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesite Vama najuspiješniju godinu.")
+				);
 		int brojdana = 0;
 		
 		switch (mjesec) {
-		case 1: case 3: case 5: case 7: case 8: case 10: case 12: //u svaki case staviti monthString="Sijecan" i broj dana
+		case 1:
+			monthString = "Sijećanj";
+			brojdana = 31;
+			break;
+		case 3:
+			monthString = "Ožujak";
+			brojdana = 31;
+			break;
+		case 5:
+			monthString = "Svibanj";
+			brojdana = 31;
+			break;
+		case 7:
+			monthString = "Srpanj";
+			brojdana = 31;
+			break;
+		case 8:
+			monthString = "Kolovoz";
+			brojdana = 31;
+			break;
+		case 10:
+			monthString = "Listopad";
+			brojdana = 31;
+			break;
+		case 12: //u svaki case staviti monthString="Sijecan" i broj dana
+			monthString = "Prosinac";
         	brojdana = 31;
         	break;
-        case 4: case 6: case 9: case 11:
+        case 4:
+        	monthString = "Travanj";
+        	brojdana = 30;
+        	break;
+        case 6:
+        	monthString = "Lipanj";
+        	brojdana = 30;
+        	break;
+       	case 9:
+       		monthString = "Rujan";
+        	brojdana = 30;
+        	break;
+       	case 11:
+       		monthString = "Studeni";
         	brojdana = 30;
         	break;
         case 2: if (((godina % 4 == 0) && 
@@ -25,11 +69,15 @@ public class Switch_Case02 {
                brojdana = 29;
            else
                brojdana = 28;
+        		monthString = "Veljača";
            break;
+         default:
+        	 System.out.println("Unijeli ste ne postojeći mjesec");
 
         
         	
 		}
+		System.out.println("Godine " + godina + ". u mjesecu " + monthString + " koji ima " + brojdana + " dan/a " + "vam je bilo najuspiješnije razdoblje!!!");
 		
 	}
 
